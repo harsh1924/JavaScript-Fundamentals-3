3bb9ba334fc6ccd9def3773cf58d2d4c68d48164
harsh1924 <harshrpanwar@gmail.com>
2023-08-19 20:05:41 +0530

    Fifth Assignment

---
+++ 05.js
@@ -0,0 +1,5 @@
+let radius = 23;
+let area = Math.pow(radius, 2) * Math.PI;
+console.log(`The area of the circle with radius ${radius} unit is ${area} unit square`);
+
+// OUTPUT: The area of the circle with radius 23 unit is 1661.9025137490005 unit square
